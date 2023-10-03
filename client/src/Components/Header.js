7d9b457de24b712128555d7199976078c44bc067
tamilvanan <tamilvanan@Mac-mini.local>
2023-10-03 15:41:04 +0530

    add some styles

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="text-4xl bg-yellow-300">Hello from Header</div>;
+  return <div className="text-4xl bg-yellow-300 w-[50%] mx-auto text-center font-semibold ">To-Do</div>;
 };
 
 export default Header;
--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -28,25 +28,27 @@ function Main() {
   };
 
   return (
-    <div>
-      <h1>Todo App</h1>
-      <div>
+    <div className="space-x-2 space-y-4 m-4 p-2 text-center w-[50%] mx-auto  font-serif ">
+      <h1 className=" text-xl">Todo App</h1>
+      <div  >
         {/* Input field for adding tasks */}
         <input
+        className="bg-gray-100 pl-2  rounded-sm"
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
         {/* Button to add a task */}
-        <button onClick={addTask}>Add</button>
+        <button onClick={addTask} className="bg-green-300 font-semibold rounded-sm px-2">Add</button>
       </div>
       <ul>
         {/* Map over the tasks array and display each task */}
         {tasks.map((task, index) => (
-          <li key={index}>
+          <li key={index} className="flex rounded-sm bg-amber-200 my-4 flex-wrap justify-between ml-2">
             {task}
             {/* Button to delete a task */}
-            <button onClick={() => deleteTask(index)}>Delete</button>
+            <button className="bg-red-500 text-white px-4 rounded-sm "
+            onClick={() => deleteTask(index)}>Delete</button>
           </li>
         ))}
       </ul>
